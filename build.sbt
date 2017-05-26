88b4a5e8a440f326750fa0e5bcf64fafbc39655a
Alexander Konovalov <alexander.konovalov@onefactor.com>
2017-05-26 10:56:40 +0300

    initial commit

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "spark-test"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-sql" % "2.1.1"
+      exclude("org.slf4j", "slf4j-log4j12"),
+  "ch.qos.logback" % "logback-classic" % "1.1.7"
+)
---
+++ src/main/scala/com/example/test/ParquetSimpleTest.scala
@@ -0,0 +1,26 @@
+package com.example.test
+
+import org.apache.commons.logging.LogFactory
+import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.slf4j.LoggerFactory
+
+/**
+  * Created by alexander.konovalov on 26.05.17.
+  */
+object ParquetSimpleTest extends App {
+
+  private val logger = LoggerFactory.getLogger(getClass)
+
+  val spark = SparkSession
+    .builder()
+    .appName("Spark Test parquet writing")
+    .master("local[1]")
+    .getOrCreate()
+
+  val peopleDF = spark.read.json("data/people.json")
+  peopleDF.show()
+
+  logger.info("Start writing...")
+  peopleDF.write.mode(SaveMode.Overwrite).parquet("data/people.parquet")
+  logger.info("Finished")
+}
