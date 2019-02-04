5720b62855725c89edfce1b6125cc6d6f2f1c6c5
Alexander Konovalov <alexander.konovalov@onefactor.com>
2019-02-04 18:02:36 +0300

    ks-test: add cdfByQuantiles function

--- src/main/scala/com/example/test/dq/KSStatsJob.scala
+++ src/main/scala/com/example/test/dq/KSStatsJob.scala
@@ -2,13 +2,14 @@ package com.example.test.dq
 
 import org.apache.spark.mllib.stat.Statistics
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.DoubleType
 
 // spark-submit --class com.onefactor.dq.KSStatsJob dq-ks-stats-assembly-1.0.jar alltran_count_in_out_alltime_alldays_d28 /Users/alexander.konovalov/Developer/scala/education/spark-test/data/all_network_counters_daily_d28/dp=megafon/year=2019/month=01/day=29 /Users/alexander.konovalov/Developer/scala/education/spark-test/data/all_network_counters_daily_d28/dp=megafon/year=2019/month=01/day=30 /Users/alexander.konovalov/Developer/scala/education/spark-test/data/all_network_counters_daily_d28/dp=megafon/year=2019/month=01/day=30/_ks_test_result_d1
 
 object KSStatsJob {
 
   val relative_error = 0.0001
-  val probabilities = (1 until 10).map(_.toDouble / 10).toArray
+  val probabilities = (0 to 100).map(_.toDouble / 100).toArray
 
   def main(args: Array[String]): Unit = {
 
@@ -31,17 +32,20 @@ object KSStatsJob {
     val df1 = spark.read.parquet(pathPrev)
     val df2 = spark.read.parquet(pathCurr)
 
-    val quantiles1 = df1.stat.approxQuantile(colName, probabilities , relative_error).toSeq
-    val quantiles2 = df2.stat.approxQuantile(colName, probabilities , relative_error).toSeq
+    val quantiles = df2.stat.approxQuantile(colName, probabilities , relative_error)
 
     println("Approx quantile:")
-    println(s"quantiles1 = $quantiles1")
-    println(s"quantiles2 = $quantiles2")
+    println(s"quantiles = ${quantiles.toSeq}")
+    println(s"probabilities = ${probabilities.toSeq}")
 
     // perform a KS test using a cumulative distribution function of our making
-    val myCDF = quantiles1.zip(quantiles2).toMap
-    println(s"myCDF = ${myCDF}")
-    val testResult = Statistics.kolmogorovSmirnovTest(spark.sparkContext.parallelize(quantiles1), myCDF)
+    import spark.implicits._
+    val inputRdd = df1
+      .select(colName)
+      .withColumn(colName, df1(colName).cast(DoubleType))
+      .map(_.getAs[Double](0))
+      .rdd
+    val testResult = Statistics.kolmogorovSmirnovTest(inputRdd, cdfByQuantiles(probabilities, quantiles)(_))
     println(testResult)
 
     // write result
@@ -55,6 +59,24 @@ object KSStatsJob {
       .csv(outputPath)
 
     resDF.show(false)
+  }
+
+  def cdfByQuantiles(probabilities: Array[Double], quantiles: Array[Double])(value: Double): Double = {
+    if (quantiles.contains(value)) {
+      val index = quantiles.lastIndexOf(value)
+      probabilities(index)
+    } else {
+      val rightIndex = quantiles.indexWhere(value < _)
+      if (rightIndex == -1) // return prob for max quantile
+        probabilities.last
+      else if (rightIndex == 0) // return prob for min quantile
+        probabilities.head
+      else {
+        val leftIndex = rightIndex - 1
+        val weight = (value - quantiles(leftIndex)) / (quantiles(rightIndex) - quantiles(leftIndex))
+        (probabilities(rightIndex) - probabilities(leftIndex)) * weight + probabilities(leftIndex)
+      }
+    }
   }
 }
 case class KSResult(colName: String, pValue: Double, statistic: Double, nullHypothesis: String)
